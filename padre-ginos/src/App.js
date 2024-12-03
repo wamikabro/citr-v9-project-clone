b3c9478b0bf185340ab41da7ea2edbe5c57d445e
Wamique Abro <wamik.abro212@gmail.com>
2024-12-03 20:08:11 +0500

    first time react

---
+++ padre-ginos/src/App.js
@@ -0,0 +1,11 @@
+const App = () => {
+  return React.createElement(
+    "div",
+    {},
+    React.createElement("h1", {}, "Padre Gino's")
+  );
+};
+
+const container = document.querySelector("#root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
