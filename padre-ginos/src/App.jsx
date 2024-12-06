12a64e4021733f32aeafb96c9d269453f373209c
Wamique Abro <wamik.abro212@gmail.com>
2024-12-06 10:00:33 +0500

    style the order page and form

--- padre-ginos/src/App.jsx
+++ padre-ginos/src/App.jsx
@@ -1,25 +1,11 @@
 import { createRoot } from "react-dom/client";
-import Pizza from "./Pizza";
+import Order from "./Order";
 
 const App = () => {
   return (
     <div>
       <h1>Padre Gino's - Order</h1>
-      <Pizza
-        name="Margherita"
-        description="Tomato sauce, mozzarella, basil, and oregano"
-        image="/public/pizzas/pepperoni.webp"
-      />
-      <Pizza
-        name="Prosciutto-Marinara"
-        description="Prosciutto, tomatoes, olive oil, garlic, and basil"
-        image="/public/pizzas/hawaiian.webp"
-      />
-      <Pizza
-        name="Funghi-Antipasto"
-        description="Grated Parmesan cheese, cherry tomatoes, basil, and oregano"
-        image="/public/pizzas/big_meat.webp"
-      />
+      <Order />
     </div>
   );
 };
