04428355996b736798cb78106c21b8dd8acbe481
Wamique <wamik.abro212@gmail.com>
2024-12-26 18:45:14 +0500

    add pizza of the day to the website

--- padre-ginos/src/App.jsx
+++ padre-ginos/src/App.jsx
@@ -1,11 +1,13 @@
 import { createRoot } from "react-dom/client";
 import Order from "./Order";
+import PizzaOfTheDay from "./PizzaOfTheDay";
 
 const App = () => {
   return (
     <div>
       <h1>Padre Gino's - Order</h1>
       <Order />
+      <PizzaOfTheDay />
     </div>
   );
 };
