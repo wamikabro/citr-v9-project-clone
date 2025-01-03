79166fa2b74184efcb01dd9b06898efa78ef58d5
Wamique <wamik.abro212@gmail.com>
2025-01-04 00:47:47 +0500

    navigate to home when header clicked

--- padre-ginos/src/Header.jsx
+++ padre-ginos/src/Header.jsx
@@ -1,12 +1,15 @@
 import { useContext } from "react";
 import { CartContext } from "./contexts";
-
+import { Link } from "@tanstack/react-router";
 
 export default function Header(){
     const [cart] = useContext(CartContext)
     return (
         <nav>
+            <Link to="/">
             <h1 className="logo">Padre Gino's</h1>
+            </Link>
+            
             <div className="nav-cart">
                 🛒<span className="nav-cart-number">{cart.length}</span>
             </div>
