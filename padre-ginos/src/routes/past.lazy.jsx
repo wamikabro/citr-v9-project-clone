b00c37f1b2a94b83ee2b66fe950aea991c091381
Wamique <wamik.abro212@gmail.com>
2025-01-11 07:48:55 +0500

    fetch past order details

--- padre-ginos/src/routes/past.lazy.jsx
+++ padre-ginos/src/routes/past.lazy.jsx
@@ -2,16 +2,28 @@ import { createLazyFileRoute } from '@tanstack/react-router'
 import { useState } from 'react'
 import { useQuery } from '@tanstack/react-query'
 import getPastOrders from '../api/getPastOrders'
+import getPastOrder from '../api/getPastOrder'
+import Modal from '../Modal'
+import {priceConverter} from '../useCurrency'
+
 export const Route = createLazyFileRoute('/past')({
   component: PastOrdersRoute,
 })
 
 function PastOrdersRoute() {
   const [page, setPage] = useState(1);
+  const [focusedOrder, setFocusedOrder] = useState(null);
   const {isLoading, data} = useQuery({
     queryKey: ['past-orders', page],  
     queryFn: () => getPastOrders(page),
     staleTime: 30000,
+  });
+
+  const {isLoading: isLoadingPastOrder, data: pastOrderData} = useQuery({
+    queryKey: ['past-order', focusedOrder],
+    queryFn: () => getPastOrder(focusedOrder),
+    staleTime: 24 * 60 * 60 * 1000,
+    enabled: !!focusedOrder,
   });
 
   if(isLoading){
@@ -35,7 +47,9 @@ function PastOrdersRoute() {
         <tbody>
           {data.map(order => ( // it shows return implicitly that's why we used () instead of {}
             <tr key={order.order_id}>
-              <td>{order.order_id}</td>
+              <td>
+                <button onClick={() => setFocusedOrder(order.order_id)}>{order.order_id}</button>
+              </td>
               <td>{order.date}</td>
               <td>{order.time}</td>
             </tr>
@@ -46,6 +60,42 @@ function PastOrdersRoute() {
         <button disabled={page <=1} onClick={()=> setPage(page - 1)}>Previous</button>
         <button disabled={data.length < 10} onClick={()=> setPage(page + 1)}>Next</button>
       </div>
+      {
+        focusedOrder ? (
+          <Modal>
+            <h2>Order #(focusedOrder)</h2>
+            {!isLoadingPastOrder ? (
+              <table>
+                <thead>
+                  <tr>
+                    <td>Image</td>
+                    <td>Name</td>
+                    <td>Size</td>
+                    <td>Quantity</td>
+                    <td>Price</td>
+                    <td>Total</td>
+                  </tr>
+                </thead>
+                <tbody>
+                    {pastOrderData.orderItems.map((pizza) => (
+                      // this key is for reordering the same list. Not in our use btw
+                      <tr key={`${pizza.pizzaTypeId}_${pizza.size}`}>
+                        <td><img src={pizza.image} alt={pizza.name} /></td>
+                        <td>{pizza.name}</td>
+                        <td>{pizza.size}</td>
+                        <td>{pizza.quantity}</td>
+                        <td>{priceConverter(pizza.price)}</td>
+                        <td>{priceConverter(pizza.total)}</td>
+                      </tr>
+                  ))}
+                </tbody>
+              </table>
+            ) : (<p>LOADING ...</p>)}
+            {/*unrendering by simply making FocusedOrder null*/}
+            <button onClick={() => setFocusedOrder()}>Close</button> 
+          </Modal>
+        ) : null // we could've done <></> instead of null
+      }
     </div>
   );
 
